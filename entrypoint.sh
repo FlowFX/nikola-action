d55ba95cd073eda6552299ffdd5d795a22e137be
Florian Posdziech <hallo@flowfx.de>
2022-09-27 21:07:45 +0200

    Reduce build & deploy script to nikola deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,37 +7,15 @@ set -e
 echo "REPO: $GITHUB_REPOSITORY"
 echo "ACTOR: $GITHUB_ACTOR"
 
-echo "==> Preparing..."
-if ! $INPUT_DRY_RUN; then
-    src_branch="$(python -c 'import conf; print(conf.GITHUB_SOURCE_BRANCH)')"
-    dest_branch="$(python -c 'import conf; print(conf.GITHUB_DEPLOY_BRANCH)')"
-    
-    git remote add ghpages "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-    git fetch ghpages $dest_branch
-    git checkout -b $dest_branch --track ghpages/$dest_branch || true
-    git pull ghpages $dest_branch || true
-    git checkout $src_branch
-    
-    # Override config so that ghp-import does the right thing.
-    printf '\n\nGITHUB_REMOTE_NAME = "ghpages"\nGITHUB_COMMIT_SOURCE = False\n' >> conf.py
-else
-    echo "Dry-run, skipping..."
-fi
-
 echo "==> Installing requirements..."
-if [[ -f "requirements.txt" ]]; then
-    # Since people might type just 'nikola', we force ghp-import to be installed.
-    pip install -r requirements.txt ghp-import
-else
-    pip install "Nikola[extras]"
-fi
+pip install -r requirements.txt
 
 echo "==> Building site..."
 nikola build
 
 echo "==> Publishing..."
 if ! $INPUT_DRY_RUN; then
-    nikola github_deploy
+    nikola deploy
 else
     echo "Dry-run, skipping..."
 fi
